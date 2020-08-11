4870af26b924416af18bf8e1aa05b1a28b4e5ade
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-11 17:34:13 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,21 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
+
+  def change
+    ActiveRecord::Base.establish_connection(
+      :adapter => "sqlite3",
+      :database => "db/artists.sqlite"
+    )
+
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS artists (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      genre TEXT,
+      age INTEGER,
+      hometown TEXT
+      )
+    SQL
+
   end
 
-  def down
-  end
 end
